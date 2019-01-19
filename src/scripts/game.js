0a2ca7b8e7c9bb31906df7e95c9993e571fbf1a0
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2019-01-19 23:06:46 +0100

    some controls

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,25 +1,59 @@
 import * as PIXI from "pixi.js";
 import Tank from "./tank.js"
 
-export default class Game {
+export default class Game extends PIXI.utils.EventEmitter {
   constructor(element) {
+    super();
     this._element = element;
     this.tanks = [];
+    this.userPlayer = null;
     this.stage = new PIXI.Container();
-    this.renderer = PIXI.autoDetectRenderer( window.innerWidth, window.innerHeight, {transparent: true}, false );
-    this._element.appendChild( this.renderer.view );
+    this.renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, { transparent: true }, false);
+    this._element.appendChild(this.renderer.view);
     this._lastFrameTime = 0;
+    this.bindInput();
     requestAnimationFrame(this._tick.bind(this));
   }
   _tick(currentTime) {
-    // this.emit( 'update', currentTime - this._lastFrameTime, currentTime );
-		this._lastFrameTime = currentTime;
-		this.renderer.render( this.stage );
-		requestAnimationFrame( this._tick.bind( this ) );
+    this.emit('update', currentTime - this._lastFrameTime, currentTime);
+    this._lastFrameTime = currentTime;
+    this.renderer.render(this.stage);
+    requestAnimationFrame(this._tick.bind(this));
   }
-  addPlayer( ) {
-		var x = this.renderer.width * ( 0.1 + Math.random() * 0.8 );
-		var y = this.renderer.height * ( 0.1 + Math.random() * 0.8 );
-		this.tanks.push( new Tank( this, x, y ) );
-	}
+  addPlayer() {
+    var x = this.renderer.width * (0.1 + Math.random() * 0.8);
+    var y = this.renderer.height;
+    this.userPlayer = new Tank(this, x, y);
+    this.tanks.push(this.userPlayer);
+  }
+  bindInput() {
+    var self = this;
+
+    const handleKeyDown = (evt) => {
+      switch (evt.keyCode) {
+        case 37:
+          self.userPlayer.turnLeft();
+          return;
+        case 39:
+          self.userPlayer.turnRight();
+          return;
+        case 32:
+          self.userPlayer.break();
+          return;
+      }
+    }
+    const handleKeyUp = (evt) => {
+      switch (evt.keyCode) {
+        case 37:
+        case 39:
+          self.userPlayer.stopTurn();
+          return;
+        case 32:
+          self.userPlayer.stopBreak();
+          return;
+      }
+    }
+    this._element.addEventListener('keydown', handleKeyDown);
+    this._element.addEventListener('keyup', handleKeyUp);
+  }
 }
--- src/scripts/tank.js
+++ src/scripts/tank.js
@@ -1,16 +1,76 @@
 import * as PIXI from "pixi.js";
 
+const ACCELERATION = .001;
+const TURNACCELERATION = .1;
+const MAX_SPEED = 15;
+
 export default class Tank {
   constructor(game, x, y) {
     this._game = game;
+
+    this._container = new PIXI.Container();
+		this._container.position.x = x;
+    this._container.position.y = y;
+
     this._body = PIXI.Sprite.fromImage("../assets/tankBlue.png");
-    this._body.position.x = x;
-    this._body.position.y = y;
     this._body.anchor.x = 0.5;
     this._body.anchor.y = 0.5;
-    this._game.stage.addChild(this._body);
+    this._container.addChild( this._body );
+
+    this._game.stage.addChild(this._container);
+
+    this._speed = 0;
+    this._turnLeft = false;
+    this._turnRight = false;
+    this._break = false;
+
+    this._game.on('update', this._update.bind(this));
   }
-  _update( msSinceLastFrame, currentTime ) {
-    console.log("woop woop");
+  _update(msSinceLastFrame, currentTime) {
+
+    if(this._break) {
+      return;
+    }
+
+    if(this._turnLeft) {
+      this._container.rotation -= TURNACCELERATION;
+    }
+
+    if(this._turnRight) {
+      this._container.rotation += TURNACCELERATION;
+    }
+
+    this._speed += ( msSinceLastFrame * ACCELERATION );
+
+    if( this._speed < 0 ) {
+			this._speed = 0;
+    }
+    
+    if( this._speed > MAX_SPEED ) {
+			this._speed = MAX_SPEED;
+    }
+    if(this._container.position.y < 35) {
+      return;
+    }
+
+    this._container.position.x += Math.sin( this._container.rotation )  * this._speed;
+		this._container.position.y -= Math.cos( this._container.rotation )  * this._speed;
+  }
+  turnLeft() {
+    this._turnLeft = true;
+  }
+  turnRight() {
+    this._turnRight = true;
+  }
+  stopTurn() {
+    this._turnRight = false;
+    this._turnLeft = false;
+  }
+  break() {
+    this._break = true;
+    this._speed = 0;
+  }
+  stopBreak() {
+    this._break = false;
   }
 }
